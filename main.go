3edc0a5f99ba9b19d2983039b80f43b85cdf1e79
Johannes Welsch <welschmoor@gmail.com>
2021-11-20 05:41:58 +0100

    getting num of pages and urls working

--- main.go
+++ main.go
@@ -2,45 +2,57 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-type requestResult struct {
-	url    string
-	status string
-}
+//
+var baseURL string = "https://www.ebay-kleinanzeigen.de/s-50321/fahrrad-pegasus/k0l1158r10"
 
 func main() {
-	c := make(chan requestResult)
-	results := make(map[string]string)
-	urls := []string{
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-		"https://www.airbnb.com",
-	}
+	totalPages := getPages()
 
-	for _, url := range urls {
-		go VisitURL(url, c)
-	}
-
-	for i := 0; i < len(urls); i++ {
-		result := <-c
-		results[result.url] = result.status
-	}
-
-	for url, status := range results {
-		fmt.Println(url, status)
+	for i := 0; i < totalPages; i++ {
+		getPage(i)
 	}
 }
 
-// this function visits URL using http package
-func VisitURL(url string, c chan<- requestResult) {
-	resp, err := http.Get(url)
-	if err != nil || resp.StatusCode >= 400 {
-		c <- requestResult{url: url, status: "FAILED"}
-	} else {
-		c <- requestResult{url: url, status: "OK"}
+// pages on kleinanzeigen sind in der url mit /seite:1/ und /seite:2/ usw... codiert
+func getPage(page int) {
+	pageURL := "https://www.ebay-kleinanzeigen.de/s-50321/seite:" + strconv.Itoa(page+1) + "/fahrrad-pegasus/k0l1158r200"
+	fmt.Println("pageURL:  ", pageURL)
+}
+
+// return number of pages
+func getPages() int {
+	pages := 0
+	res, err := http.Get(baseURL)
+	checkErr(err)
+	checkCode(res)
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		pages += s.Find("a").Length() // how many a-tags are there
+	})
+
+	fmt.Println(doc)
+	return pages
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err) // this kills the program
+	}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("Req failed with status: ", res.StatusCode)
 	}
 }

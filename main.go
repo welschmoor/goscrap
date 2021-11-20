95aa29b181fa4893006a89fa5ab47e7242453879
Johannes Welsch <welschmoor@gmail.com>
2021-11-20 08:29:18 +0100

    fully working csv

--- main.go
+++ main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-type extractedItem struct {
+type itemDict struct {
 	id    string
 	title string
 	price string
@@ -19,17 +21,49 @@ type extractedItem struct {
 //
 var baseURL string = "https://www.ebay-kleinanzeigen.de/s-50321/fahrrad-pegasus/k0l1158r10"
 
+// MAIN this is for all pages:
 func main() {
-	// totalPages := getPages()
+	var jobs []itemDict
+	totalPages := getPages()
 
-	// for i := 0; i < totalPages; i++ {
-	// 	getPage(i)
-	// }
-	getPage(0)
+	for i := 0; i < totalPages; i++ {
+		listingsOnSinglePage := getPage(i)
+		jobs = append(jobs, listingsOnSinglePage...)
+	}
+	// fmt.Println(jobs)
+	for _, each := range jobs {
+
+		fmt.Println(each.id, each.title, each.price)
+	}
+	saveItems(jobs)
+}
+
+// MAIN this one is for first page of results only:
+// func main() {
+// 	getPage(0) // only scrape the 1st page
+// }
+
+func saveItems(jobs []itemDict) {
+	file, err := os.Create("items.csv") // creating a file
+	checkErr(err)
+
+	w := csv.NewWriter(file)
+	defer w.Flush()
+
+	headers := []string{"ID", "Title", "Price"}
+	wErr := w.Write(headers)
+	checkErr((wErr))
+
+	for _, job := range jobs {
+		itemSlice := []string{job.id, job.title, job.price}
+		wErr := w.Write(itemSlice)
+		checkErr(wErr)
+	}
 }
 
 // pages on kleinanzeigen sind in der url mit /seite:1/ und /seite:2/ usw... codiert
-func getPage(page int) {
+func getPage(page int) []itemDict {
+	var itemList []itemDict
 	pageURL := "https://www.ebay-kleinanzeigen.de/s-50321/seite:" + strconv.Itoa(page+1) + "/fahrrad-pegasus/k0l1158r200"
 	fmt.Println("pageURL:  ", pageURL)
 
@@ -44,18 +78,25 @@ func getPage(page int) {
 
 	seachItems := doc.Find(".aditem") // .aditem is the maindiv of each posting
 	seachItems.Each(func(i int, s *goquery.Selection) {
-		id, _ := s.Attr("data-adid") // finding id of each item
-		fmt.Println(" ")
-
-		title := s.Find(".text-module-begin a").Text()
-	
-		price := s.Find(".aditem-main--middle--price").Text()
-		fmt.Println(id, title, "Price: ", strings.TrimSpace(price))
-		// extractedItem{id:id, title:title, price:price}
+		item1 := extractOneItem(s) // refactored funcction
+		itemList = append(itemList, item1)
 	})
+
+	return itemList
 }
 
+func extractOneItem(s *goquery.Selection) itemDict {
+	id, _ := s.Attr("data-adid") // finding id of each item
+	title := cleanString(s.Find(".text-module-begin a").Text())
+	price := cleanString(s.Find(".aditem-main--middle--price").Text())
 
+	// fmt.Println(id, title, "Price: ", strings.TrimSpace(price))
+	return itemDict{
+		id:    id,
+		title: title,
+		price: price}
+
+}
 
 // return number of pages
 func getPages() int {
@@ -88,6 +129,6 @@ func checkCode(res *http.Response) {
 	}
 }
 
-// func cleanString(str string) string {
-
-// }
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
+}
